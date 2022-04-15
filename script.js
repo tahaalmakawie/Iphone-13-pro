3361c0bdcaa277b9b2b934ba56cb124e533ecb9e
tahaalmakawie <tahaalmakawie@gmail.com>
2022-04-14 20:40:59 -0700

    Iphones

---
+++ script.js
@@ -0,0 +1,9 @@
+let img0 = document.querySelector(".img0");
+let container = document.querySelector(".container");
+
+function phones(phone){
+    img0.src = phone
+}
+function colors(color){
+    container.style.background = color;
+}
